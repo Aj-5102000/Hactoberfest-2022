29f792a0a2c8a0b2cac7965b7b28271892feb7df
Aj-5102000 <114498555+Aj-5102000@users.noreply.github.com>
2022-10-06 14:57:59 +0530

    Bitonic_Sort.cpp

---
+++ C++/Sorting/Bitonic_Sort.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void compAndSwap(int a[], int i, int j, int dir)
+{
+	if (dir==(a[i]>a[j])) swap(a[i],a[j]);
+}
+
+void bitonicMerge(int a[], int low, int cnt, int dir)
+{
+	if (cnt>1) {
+		int k = cnt/2;
+		for (int i=low; i<low+k; i++) compAndSwap(a, i, i+k, dir);
+		bitonicMerge(a, low, k, dir);
+		bitonicMerge(a, low+k, k, dir);
+	}
+}
+
+void bitonicSort(int a[],int low, int cnt, int dir)
+{
+	if (cnt>1) {
+		int k = cnt/2;
+		bitonicSort(a, low, k, 1);
+		bitonicSort(a, low+k, k, 0);
+		bitonicMerge(a,low, cnt, dir);
+	}
+}
+
+void sort(int a[], int N, int up)
+{
+	bitonicSort(a,0, N, up);
+}
+
+int main()
+{
+	int a[]= {13, 87, -4, 28, 66, 2, 11, 5};
+	int N = sizeof(a)/sizeof(a[0]);
+
+	int up = 1;
+	sort(a, N, up);
+
+	printf("Sorted  : );
+	for (int i=0; i<N; i++)
+		printf("%d ", a[i]);
+	return 0;
+}
